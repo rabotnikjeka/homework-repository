3cb8a765c7e5d7ef783df5a58c5d2de752d08710
Evgeniy Fedorov <ev.fedorovv@gmail.com>
2025-10-01 16:32:42 +0300

    homework operators fix

--- JavaScript/operators/operators.js
+++ JavaScript/operators/operators.js
@@ -1,15 +1,14 @@
 let a = Number(prompt("Введите ваше число"));
 const c = 2;
 let result;
-let chekResult;
-function chek(obj) {
+let checkResult;
+function check(obj) {
   return obj === 0;
 }
 result = a % c;
-chekResult = chek(result);
-if (chekResult === true) console.log(a + " - четное число");
+checkResult = check(result);
+if (checkResult) console.log(a + " - четное число");
 else console.log(a + " - нечетное число");
-console.log();
 let userName = prompt("Как вас зовут?");
 let spaceName = "Guest";
 console.log("Hello, " + (userName || spaceName) + "!");
