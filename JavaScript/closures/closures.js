c41fa9fd8420a8855ac3b92b60acc274525e6913
Evgeniy Fedorov <ev.fedorovv@gmail.com>
2025-11-16 17:28:07 +0300

    homework closures ver2

--- JavaScript/closures/closures.js
+++ JavaScript/closures/closures.js
@@ -31,17 +31,69 @@ showAnna();
 
 // Третье задание
 
-const n = Number(prompt());
+function fibonacciNumbers() {
+  const cache = {};
 
-function theBinetFormulaConst() {
-  const x = (1 + Math.sqrt(5)) / 2;
-  const y = (1 - Math.sqrt(5)) / 2;
-  function fibonacciNumberFunction() {
-    let fn = (x ** n - y ** n) / Math.sqrt(5);
-    console.log(Math.round(fn));
-  }
-  return fibonacciNumberFunction;
+  return function (n) {
+    if (cache[n] !== undefined) {
+      console.log("Из кэша:", cache[n], ` Искомое число Фибоначчи - ${n}`);
+      return cache[n];
+    }
+
+    const x = (1 + Math.sqrt(5)) / 2;
+    const y = (1 - Math.sqrt(5)) / 2;
+
+    const fibonacciNumber = Math.round((x ** n - y ** n) / Math.sqrt(5));
+
+    cache[n] = fibonacciNumber;
+    console.log("Вычислено:", fibonacciNumber, ` Искомое число Фибоначчи - ${n}`);
+    return fibonacciNumber;
+  };
 }
 
-const fibonacciNumber = theBinetFormulaConst();
-fibonacciNumber();
+const fib = fibonacciNumbers();
+
+fib(5);
+fib(10);
+fib(15);
+fib(10);
+fib(5);
+// реализовал вариант без цикла тк в мире уже существует формула для определения конкртного числа Фибоначчи), но добавил замыкание и кэш
+
+function FibonacciCalculator() {
+  const cache = {};
+
+  return function (n) {
+    if (cache[n] !== undefined) {
+      console.log(`Из кэша: Искомое число Фибоначчи(${n}) = ${cache[n]}`);
+      return cache[n];
+    }
+
+    let result;
+    if (n === 0) result = 0;
+    else if (n === 1) result = 1;
+    else {
+      let previously = 0;
+      let current = 1;
+      for (let i = 2; i <= n; i++) {
+        const next = previously + current;
+        previously = current;
+        current = next;
+      }
+      result = current;
+    }
+
+    cache[n] = result;
+    console.log(`Вычислено: Искомое число Фибоначчи(${n}) = ${result}`);
+    return result;
+  };
+}
+
+const fibonacci = FibonacciCalculator();
+fibonacci(5);
+fibonacci(10);
+fibonacci(15);
+fibonacci(10);
+fibonacci(5);
+
+// Я думаю что то такое ожидалось по заданию, не без помощи гугла но что то похожее на правду получилось)
