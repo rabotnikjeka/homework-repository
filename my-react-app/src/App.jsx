6d9fb38cc652fd1d94ea55024c3ec129517366c5
Evgeniy Fedorov <ev.fedorovv@gmail.com>
2026-02-28 20:22:14 +0300

    homework optimization

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,30 +1,7 @@
 import "./App.css";
-import { useContext } from "react";
-import { AuthContext, AuthProvider } from "./AuthContext";
 
-function Content() {
-  const { isLoggedIn, login } = useContext(AuthContext);
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      {isLoggedIn ? (
-        <p style={{ fontSize: "24px", color: "green" }}>Вы авторизованы!</p>
-      ) : (
-        <button
-          onClick={login}
-          style={{ padding: "10px 20px", fontSize: "18px" }}
-        >
-          Войти
-        </button>
-      )}
-    </div>
-  );
+function App() {
+  return <div></div>;
 }
 
-export function App() {
-  return (
-    <AuthProvider>
-      <Content />
-    </AuthProvider>
-  );
-}
+export default App;
