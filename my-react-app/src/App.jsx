10aaa8f6a295321387839b452a3a5151c9981918
Evgeniy Fedorov <ev.fedorovv@gmail.com>
2026-04-04 13:46:29 +0300

    homework lazy&suspense

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
-
-import UserCard from "./UserCard";
+import React, { Suspense, lazy, useState } from "react";
+const BigComponent = lazy(() => import("./BigComponent"));
 
 function App() {
+  const [isButtonClick, setIsButtonClick] = useState(false);
+
   return (
     <div>
-      <h1>Компонент высшего порядка</h1>
-      <UserCard />
+      <h1>Lazy & Suspense</h1>
+      <Suspense fallback={<p>Loading ...</p>}>
+        {isButtonClick ? <BigComponent /> : null}
+      </Suspense>
+
+      {!isButtonClick ? (
+        <button
+          onClick={() => {
+            setIsButtonClick(true);
+          }}
+        >
+          Show
+        </button>
+      ) : null}
     </div>
   );
 }
---
+++ my-react-app/src/BigComponent.jsx
@@ -0,0 +1,10 @@
+export default function BigComponent() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque inventore,
+      sint nihil deleniti sequi dolores ut magni, reprehenderit quasi
+      dignissimos architecto voluptatibus ipsa aut voluptas quia laborum.
+      Veniam, eligendi quisquam!
+    </div>
+  );
+}
